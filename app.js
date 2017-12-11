6954ebacd42b3b45d629f83318b6bffd68b7d34b
shawn <axe2781@gmail.com>
2017-12-11 15:07:40 -0500

    pp

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+const http = require('http');
+
 const server = http.createServer();
 
 server.on('request', (request, response) => {
-  const { method, url } = request;
-  const { method, header } = request;
-  const userAgent = header['user=agent'];
+  const { method, url, headers } = request;
+  const userAgent = headers['user=agent'];
 
   if (url == "/") {
     response.statusCode = 200;
@@ -11,7 +12,7 @@ server.on('request', (request, response) => {
     console.log("Received Request");
     response.write('<html><head><h1><title>Hello</title></head><body><h1>Hello to my World!</h1></body></html>');
     response.end();
-  } else {
+  }  else {
     response.statusCode = 404;
     response.write('404: File Not Found!');
     response.end();
