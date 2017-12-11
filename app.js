dc603e92dd42cfbda86f2a8939360b4109401d0d
shawn <axe2781@gmail.com>
2017-12-11 12:04:35 -0500

    f

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const server = http.createServer();
+
 server.on('request', (request, response) => {
-  
+  const {method, url } = request;
+  const {method, header} = request;
+  const userAgent = header['user=agent'];
+
+  if (url == "/") {
+      response.statusCode = 200;
+
+      
+  }
+
 });
