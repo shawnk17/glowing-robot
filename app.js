cd586bd3558c3ce44ba114bf60c2c0a2ce62ec09
shawn <axe2781@gmail.com>
2017-12-11 13:20:32 -0500

    appp

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const server = http.createServer();
 
 server.on('request', (request, response) => {
-  const {method, url } = request;
-  const {method, header} = request;
+  const { method, url } = request;
+  const { method, header } = request;
   const userAgent = header['user=agent'];
 
   if (url == "/") {
-      response.statusCode = 200;
+    response.statusCode = 200;
 
-      
+    console.log("Received Request");
+    response.write('<html><head><h1><title>Hello</title></head><body><h1>Hello to my World!</h1></body></html>');
+    response.end();
+  } else {
+    response.statusCode = 404;
+    response.write('404: File Not Found!');
+    response.end();
   }
+});
 
+server.listen(3000, () => {
+  console.log("Success! Listening on port 3000");
 });
